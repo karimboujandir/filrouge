5dd37eb06d99899f23a51377e8e2e3a90d629a6f
karimboujandir <118886991+karimboujandir@users.noreply.github.com>
2023-03-12 18:16:44 +0100

    Add files via upload

---
+++ filrouge2-master/filrouge2-master/fil-rouge-master/fil-rouge-master/teste/filrouge.js
@@ -0,0 +1,17 @@
+
+/* connexion*/
+const signUpButton = document.getElementById('signUp');
+const signInButtom = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+  container.classList.add('right-panel-active');
+});
+
+signInButtom.addEventListener('click', () => {
+  container.classList.remove('right-panel-active');
+});  
+
+
+
+/* carousel */
